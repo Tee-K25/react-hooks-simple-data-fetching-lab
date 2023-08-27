457ac32df23a02babd97fa7eee5c0481160becdd
Tony <tony.kiplagat@student.moringaschool.com>
2023-08-27 10:56:28 +0300

    Fetch lab done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import { useState, useEffect } from "react";
+const App = () => {
+  const [dog, setDog] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => {
+        setDog(data);
+        setIsPending(false);
+      });
+  }, []);
+  return (
+    <div className="main">
+      {isPending && <p>Loading...</p>}
+      {dog && <img src={dog} alt="A Random Dog" />}
+    </div>
+  );
+};
+
+export default App;
